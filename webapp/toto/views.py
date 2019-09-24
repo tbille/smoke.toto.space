1c1bd36679cce0203e1b9faf4c5458c0ebd2c971
Thomas Bille <thomas.bille@canonical.com>
2019-09-24 16:39:08 +0100

    fix date

--- webapp/toto/views.py
+++ webapp/toto/views.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from flask import Blueprint, render_template
+
 
 toto = Blueprint(
     "toto", __name__, template_folder="/templates", static_folder="/static"
@@ -7,9 +9,7 @@ toto = Blueprint(
 
 @toto.route("/")
 def index():
-    from datetime import datetime
-
-    d0 = datetime(2019, 9, 23)
+    d0 = datetime(year=2019, month=9, day=22, hour=21)
     d1 = datetime.now()
     delta = d1 - d0
     return render_template("toto/index.html", smokedate=delta)
